9901c5023bc108c5bed374771a6c1b75a0bc8276
Yoshihiro4218 <nana.mizuki77@icloud.com>
2020-05-05 12:47:33 +0900

    create backend

---
+++ backend/src/test/java/com/yoshi/images/ImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoshi.images;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
