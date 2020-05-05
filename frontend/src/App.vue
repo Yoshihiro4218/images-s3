7e58e083500772acba3709b6bbdb8bca7eb6463b
Yoshihiro4218 <nana.mizuki77@icloud.com>
2020-05-05 21:52:23 +0900

    とりあえずimageを全件表示

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img style="margin-bottom: 30px;" alt="Vue logo" src="./assets/logo.png">
+    <div style="margin-bottom: 10px; border-bottom: aqua solid 3px;"></div>
+    <div style="width: 750px; text-align: center; margin-left: 350px;">
+      <div v-for="(item, idx) of items" :key="idx" style="display: inline-flex;">
+        <img :src="'data:image/jpeg;base64,' + item" alt="image"
+             style="margin-right: 50px; width: 200px; margin-top: 50px;">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      items: [],
+    }
+  },
+  mounted() {
+    axios
+      .get('/api/images')
+      .then((res) => {
+        this.items = res.data
+      })
+  },
+  methods: {}
 }
 </script>
 
@@ -23,6 +39,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import VueLazyload from 'vue-lazyload';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
+
+// Vue.use(VueLazyload, {
+//   preLoad: 1.3, // 事前ロードする高さの割合指定
+//   error: 'images/error.png', // エラー時に表示する画像指定
+//   loading: 'images/loading.gif', // ロード中に表示する画像指定
+//   attempt: 1 // ロード失敗した時のリトライの上限指定
+// });
